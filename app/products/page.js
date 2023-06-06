3b053ac679d820890eec4390d9c6d674f65d363e
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-06-06 18:54:39 +0200

    Change image resolution

--- app/products/page.js
+++ app/products/page.js
@@ -12,31 +12,30 @@ export const dynamic = 'force-dynamic';
 export default async function ProductsPage() {
   const products = await getProducts();
   return (
-    <main className="structureFlex basicFlexVertical">
-      <h1 className="bottomGap cartPaddingGlobal">Our products:</h1>
-      {products.map((product) => {
-        return (
-          <section
-            key={`product-div-${product.id}`}
-            className="basicFlex cartPaddingGlobal bottomGap"
-          >
-            <div className="basicFlex cartSpaceBetween cartBorder bottomPaddingHalf">
-              <Image
-                src={`/images/${product.name}_product.png`}
-                width={300}
-                height={300}
-                alt="Product pic"
-              />
-              <Link
-                data-test-id={`product-${product.id}`}
-                href={`/products/${product.id}`}
-              >
-                <h1>{product.name}</h1>
-              </Link>
+    <main className="structureFlex basicFlexVertical cartPaddingGlobal bottomGap">
+      <h1 className="bottomGap">Our products:</h1>
+      {/* evtl grid hier */}
+      <section className="listFlex">
+        {products.map((product) => {
+          return (
+            <div key={`product-div-${product.id}`} className="listFlexItem">
+              <div className="contentFlex basicFlexVertical bottomPaddingHalf">
+                <img
+                  src={`/images/${product.name}_product_sm.jpg`}
+                  alt="Product pic"
+                />
+                <Link
+                  data-test-id={`product-${product.id}`}
+                  href={`/products/${product.id}`}
+                >
+                  <h1 className="bottomPadding">{product.name}</h1>
+                </Link>
+                <p>{product.description}</p>
+              </div>
             </div>
-          </section>
-        );
-      })}
+          );
+        })}
+      </section>
     </main>
   );
 }
