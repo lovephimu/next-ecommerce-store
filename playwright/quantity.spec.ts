3d530e35158338d2ecb44cd61dafbe6fd485a664
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-06-06 14:22:49 +0200

    Change playwright and add grey pics

--- playwright/quantity.spec.ts
+++ playwright/quantity.spec.ts
@@ -30,7 +30,7 @@ test('quantity manipulation test', async ({ page }) => {
   await page.getByRole('link', { name: 'Cart' }).click();
   await expect(page).toHaveURL('http://localhost:3000/cart');
 
-  await expect(page.getByTestId('cart-product-quantity-1')).toHaveText('1');
+  await expect(page.getByTestId('cart-product-quantity-1')).toHaveText(/1/);
   await page.getByTestId('cart-product-remove-1').click();
   await expect(page.getByTestId('cart-product-quantity-1')).not.toBeVisible;
 });
