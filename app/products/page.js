bf96a23e832273192c8db1addd5c8e6b4e6383a7
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-06-06 11:06:39 +0200

    Delete lock file

--- app/products/page.js
+++ app/products/page.js
@@ -33,7 +33,6 @@ export default async function ProductsPage() {
               >
                 <h1>{product.name}</h1>
               </Link>
-              <p>short description</p>
             </div>
           </section>
         );
